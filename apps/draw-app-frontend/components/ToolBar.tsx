a8f41b4105e9551fe48fe95064c1fbcb9708f4b2
Fahad-kidwai <fahadkidwai27@gmail.com>
2026-01-18 00:00:20 +0530

    Enhance toolbar UI and placement

--- apps/draw-app-frontend/components/ToolBar.tsx
+++ apps/draw-app-frontend/components/ToolBar.tsx
@@ -1,34 +1,47 @@
-import { Circle, Eraser, Pencil, RectangleHorizontalIcon } from "lucide-react"
-import { IconButton } from "./IconButton"
+import { Circle, Eraser, Pencil, RectangleHorizontalIcon } from "lucide-react";
+import { IconButton } from "./IconButton";
 
-type Tool = "circle" | "rect" | "pencil"|"erase"
+type Tool = "circle" | "rect" | "pencil" | "erase";
 
-export function ToolBar({selectedTool, setSelectedTool}: {
-    selectedTool: Tool,
-    setSelectedTool: (s: Tool) => void
+export function ToolBar({
+  selectedTool,
+  setSelectedTool,
+}: {
+  selectedTool: Tool;
+  setSelectedTool: (s: Tool) => void;
 }) {
-    return <div style={{
-            position: "fixed",
-            top: 10,
-            left: 10
-        }}>
-            <div className="flex gap-t">
-                <IconButton 
-                    onClick={() => {
-                        setSelectedTool("pencil")
-                    }}
-                    activated={selectedTool === "pencil"}
-                    icon={<Pencil />}
-                />
-                <IconButton onClick={() => {
-                    setSelectedTool("rect")
-                }} activated={selectedTool === "rect"} icon={<RectangleHorizontalIcon />} ></IconButton>
-                <IconButton onClick={() => {
-                    setSelectedTool("circle")
-                }} activated={selectedTool === "circle"} icon={<Circle />}></IconButton>
-                <IconButton onClick={() => {
-                    setSelectedTool("erase")
-                }} activated={selectedTool === "erase"} icon={<Eraser />}></IconButton>
-            </div>
-        </div>
-}        
+  return (
+    <div className="fixed bottom-12 left-5/12">
+      <div className="flex gap-2 bg-card px-2 py-1 rounded-lg shadow-lg">
+        <IconButton
+          onClick={() => {
+            setSelectedTool("pencil");
+          }}
+          activated={selectedTool === "pencil"}
+          icon={<Pencil />}
+        />
+        <IconButton
+          onClick={() => {
+            setSelectedTool("rect");
+          }}
+          activated={selectedTool === "rect"}
+          icon={<RectangleHorizontalIcon />}
+        ></IconButton>
+        <IconButton
+          onClick={() => {
+            setSelectedTool("circle");
+          }}
+          activated={selectedTool === "circle"}
+          icon={<Circle />}
+        ></IconButton>
+        <IconButton
+          onClick={() => {
+            setSelectedTool("erase");
+          }}
+          activated={selectedTool === "erase"}
+          icon={<Eraser />}
+        ></IconButton>
+      </div>
+    </div>
+  );
+}
